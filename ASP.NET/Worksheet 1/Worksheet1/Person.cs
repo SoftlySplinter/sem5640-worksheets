7def86e004c30ea3a81165b6b80d1826633e5826
Alex Brown <alex@alexanderdbrown.com>
2014-01-29 11:57:58 +0000

    Add ASP.NET Worksheet 1

---
+++ ASP.NET/Worksheet 1/Worksheet1/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M5640.CSharpPricatical
+{
+    class Person
+    {
+        public Person()
+        {
+            // NO-OP
+        }
+
+        public Person(string forename, string surname, int age)
+        {
+            this.Forename = forename;
+            this.Surname = surname;
+            this.Age = age;
+        }
+
+        public string Forename { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return Forename + " " + Surname + ", age: " + Age;
+        }
+    }
+}
---
+++ ASP.NET/Worksheet 1/Worksheet1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using M5640.CSharpPricatical;
+
+namespace CSharpPricatical
+{
+    class Program
+    {
+        void Run()
+        {
+            List<Person> people = new List<Person>();
+
+            #region Add Loop
+            for (int i = 0; i < 5; i++) {
+                people.Add(GenPerson());
+            }
+            #endregion
+
+            #region Print Loop
+            foreach (Person p in people)
+            {
+                Console.WriteLine("Person: " + p);
+            }
+            #endregion
+        }
+
+        Person GenPerson()
+        {
+            Console.Write("Forename: ");
+            string forname = Console.ReadLine();
+
+            Console.Write("Surname: ");
+            string surname = Console.ReadLine();
+
+            int age = GetAge();
+
+            return new Person(forname, surname, age);
+        }
+
+        int GetAge()
+        {
+            int age = 0;
+
+            #region Valid Loop
+            while (!Valid(age))
+            {
+                try
+                {
+                    Console.Write("Age: ");
+                    age = Int32.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Invalid Age");
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            #endregion
+            return age;
+        }
+
+        bool Valid(int value)
+        {
+            return value >= 1 && value <= 150;
+        }
+
+        static void Main(string[] args)
+        {
+            new Program().Run();
+            Console.ReadLine();
+        }
+    }
+}
